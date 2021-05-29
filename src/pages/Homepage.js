c479b312580db902555ccfa911c1a7ef668506bf
vayu02 <ashish02.shejwal@gmail.com>
2021-05-30 05:20:32 +0530

    added a input form  which gets the api result

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Mainlayout from '../components/Mainlayout'
 
 
 const Homepage = () => {
+
+    const [input, setinput] = useState('');
+    const chechInput = (ev) => {
+        setinput(ev.target.value)
+        // console.log(ev.target.value)
+    }
+
+    const doSearch = () => {
+        //https://api.tvmaze.com/search/shows?q=girls
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(res => res.json()).then(res =>{
+            console.log(res);
+        })
+    }
+
+    const mapKey = (ev) =>{
+        if(ev.keyCode === 13){
+            doSearch()
+        }
+    }
+
     return (
         <Mainlayout>
-            this is home
+            <input type="text" onChange={chechInput} value={input} onKeyDown={mapKey} />
+            <button type='button' onClick={doSearch} >Search</button>
+            
         </Mainlayout>
+        
     )
 }
 
